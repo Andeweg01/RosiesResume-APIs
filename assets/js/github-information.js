64a35b94a033756f1f7f18585e74a4e6ee39bd4f
Sascha Andeweg <sascha@designed.irish>
2020-02-14 20:46:02 +0000

    commit with additions to the GitHub API

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,3 +1,21 @@
+function userInformationHTML(user){
+    return `
+    <h2>${user.name}
+    <span class="small-name">
+    ($<a href="${user.html_url}" target="_blank">${user.login}</a>)
+    </span>
+    </h2>
+    <div class="gh-content">
+    <div class="gh-avatar">
+    <a href="${user.html_url} target="_blank">
+    <img src="${user.avatar_url}" width="80" heigth="80" alt="${user.login} />
+    </a>
+    </div>
+    <p>Followers: ${user.followers} - Following: ${user.following} <br>
+    Repos: ${user.public_repos}</p>
+    </div>`;
+}
+
 function fetchGitHubInformation(event){
 
     var username = $("#gh-username").val();  // jQuery to select the value in id gh-username
