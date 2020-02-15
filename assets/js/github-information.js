496b0dccd318b2fe9e8ecb65f8a0f61ae204ef90
Sascha Andeweg <sascha@designed.irish>
2020-02-15 20:59:33 +0000

    commit fixed bugs and new line bug in the repos

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -32,12 +32,14 @@ function repoInformationHTML(repos){
     <strong>Repo List:</strong>
     </p>
     <ul>
-        ${listItemsHTML.join("/n")}
+        ${listItemsHTML.join("\n")}
         </ul>
         </div>`;
 }
 
 function fetchGitHubInformation(event){
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();  // jQuery to select the value in id gh-username
     if (!username) { // if not username
@@ -76,3 +78,5 @@ function fetchGitHubInformation(event){
                 }
             });
 }
+
+$(document).ready(fetchGitHubInformation);
